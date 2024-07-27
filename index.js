78d099a67507ae68c347b9e814263a7390b186f8
Hari Chandana <gottuparthiharichandana@gmail.com>
2024-07-28 01:29:51 +0530

    created age calculator

---
+++ index.js
@@ -0,0 +1,32 @@
+const btnEl = document.getElementById("button");
+const birthdayEl = document.getElementById("dob");
+const resultEl = document.getElementById("result");
+
+function calculateAge(event) {
+  event.preventDefault();
+  const birthdayValue = birthdayEl.value;
+  if (birthdayValue === "") {
+    alert("Please enter your birthday");
+  } else {
+    const age = getAge(birthdayValue);
+    resultEl.innerText = `Your age is ${age} ${age > 1 ? "years" : "year"} old`;
+  }
+}
+
+function getAge(birthdayValue) {
+  const currentDate = new Date();
+  const birthdayDate = new Date(birthdayValue);
+  let age = currentDate.getFullYear() - birthdayDate.getFullYear();
+  const month = currentDate.getMonth() - birthdayDate.getMonth();
+
+  if (
+    month < 0 ||
+    (month === 0 && currentDate.getDate() < birthdayDate.getDate())
+  ) {
+    age--;
+  }
+
+  return age;
+}
+
+btnEl.addEventListener("click", calculateAge);
